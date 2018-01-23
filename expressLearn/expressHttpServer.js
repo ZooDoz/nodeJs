9526340f4dc9b56d07ac89ff954b766d28757bfa
ZooDoz <513828621@qq.com>
2018-01-23 10:31:35 +0800

    添加一个登录http服务和一个ws服务

---
+++ expressLearn/expressHttpServer.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var uuid = require('uuid');
+
+var app = express();
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'html')));
+
+users={};
+
+app.get('/index', function (req, res) {
+    if (req.cookies.sessionId) {
+        res.redirect('index.html?sessionId=' + req.cookies.sessionId);
+    } else {
+        res.send('<form action="login" method="get">'
+            + '<input type="submit" value="login"/></form>');
+    }
+});
+
+app.get('/login', function (req, res) {
+    if (req.cookies.sessionId) {
+        users[req.cookies.sessionId]=req.cookies.sessionId;
+        res.redirect('index.html?sessionId=' + req.cookies.sessionId);
+    }
+    else {
+        var uid = uuid.v1();
+        users[uid]=uid;
+        res.redirect('index.html?sessionId=' + uid);
+    }
+});
+
+app.get('/hasUser/:user', function (req, res) {
+    if(users.hasOwnProperty(req.params.user))
+        res.send("1");
+    else
+        res.send("0");
+});
+
+app.listen(8081);
+console.log('started on port 8081');
+
--- expressLearn/expressServer.js
+++ expressLearn/expressServer.js
@@ -1,16 +1,10 @@
 var WebSocketServer = require('ws').Server;
-var express = require('express');
 var path = require('path');
 var querystring = require('querystring');
 var http = require('http');
 
-var server = http.createServer();
-
-var app = express();
-app.use(express.static(path.join(__dirname, '/html')));
-
 var wss = new WebSocketServer({
-  server: server,
+  port: 8181,
   clientTracking: false,
   // 这是一个在创建ws时校验方法
   // verifyClient:(info , cb) =>{
@@ -22,9 +16,26 @@ wss.cm = {};
 wss.on('connection', function (ws , req) {
 
   var cookies = querystring.parse(req.headers.cookie , null , null , null);
-  console.log(cookies.sessionId);
 
-  ws.terminate();
+  http.get('http://127.0.0.1:8081/hasUser/' + cookies.sessionId, function (res) {
+    var result = "";
+    res.on('data' , function(data){
+      result+=data;
+    });
+    res.on('end' , function(){
+      if(!result)
+      {
+        var redirect = {};
+        redirect.url = 'http://127.0.0.1:8081/login';
+        ws.send(JSON.stringify(redirect));
+        ws.terminate();
+      }
+      else
+      {
+        wss[cookies.sessionId]=ws;
+      }
+    });
+  });
 
   ws.on('message' , function (d,f) {});
   ws.on('close', function (ws) {
@@ -33,7 +44,4 @@ wss.on('connection', function (ws , req) {
   ws.on('error' , function(err){});
 });
 
-server.on('request', app);
-server.listen(8081, function () {
-  console.log('Listening on http://localhost:8081');
-});
+console.log('Listening on http://localhost:8181');
