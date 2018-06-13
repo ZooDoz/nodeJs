7038a8ba090acb89069b5691b896f47d7a93331c
ZooDoz <513828621@qq.com>
2018-06-13 20:00:24 +0800

    修改成直接渲染html

--- webrtc/expressHttpServerByStatic.js
+++ webrtc/expressHttpServerByStatic.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var path = require('path');
-
+var fs = require('fs');
 
 var app = express();
 
-app.use(express.static(path.join("static","html")));
-app.use(express.static('static'));
+app.use('/js', express.static(path.join('static', 'js')));
+
+app.get('/favicon.ico', function (req, res) {
+});
 
 app.get('/:path', function (req, res) {
-        res.redirect(req.params.path + '.html');
+        var fileName = path.join(__dirname, 'static' , 'html' , req.params.path) + '.html';
+        // 从文件系统中读取请求的文件内容
+        fs.readFile(fileName, function (err, data) {
+                if (err) {
+                        console.log(err);
+                        res.sendStatus(404);
+                } else {
+                        res.type('html')
+                        // 响应文件内容
+                        res.status(200).send(data.toString());
+                }
+        });
 });
 
 app.listen(8081);
